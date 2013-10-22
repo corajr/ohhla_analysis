30aec4799cbe1a811b14907805c1c2cecf7baf28
Matthew Johnson-Roberson <mattjr@umich.edu>
2013-10-22 10:39:02 -0400

    vis plot place

--- viz_plot_place.r
+++ viz_plot_place.r
@@ -8,6 +8,7 @@ library(rgeos)
 library(grid)
 
 
+
 usmap <- map('usa', fill=TRUE, col="transparent", plot=FALSE)
 uspoly <- map2SpatialPolygons(usmap, IDs=usmap$names, 
 proj4string=CRS("+proj=longlat +datum=wgs84"))
@@ -28,9 +29,9 @@ theme_map <- function (base_size = 12, base_family = "") {
             legend.background=element_rect(fill="white", colour=NA),
             legend.key=element_rect(colour="white"),
             legend.key.size=unit(1.2, "lines"),
-            legend.position="right",
+            legend.position="bottom",
             legend.text=element_text(size=rel(0.8)),
-            legend.title=element_text(size=rel(0.8), face="bold", hjust=0),
+            legend.title=element_blank(),#element_text(size=rel(0.8), face="bold", hjust=0),
             panel.background=element_blank(),
             panel.border=element_blank(),
             panel.grid.major=element_blank(),
@@ -72,29 +73,35 @@ plotKde2d <- function(in_df){
 }
 
 density_map <- function (name) {
-    #pdf(file=paste(name, "_density.pdf", sep=''), height=4, width=5)
     datapts <- read.csv(paste(name, ".csv", sep=''))
     test <- data.frame(x=rep(datapts[,1],datapts[,3]),y=rep(datapts[,2],datapts[,3]))
     pts <- as.ppp(test, W=usowin)
     pts <-as.data.frame(pts)
     kde2dRange <- c(apply(pts[,c('x','y')], 2, range))
-
+     
+ 
     kde2dRange[1] <- kde2dRange[1] -3.0
     kde2dRange[2] <- kde2dRange[2] +3.0
     kde2dRange[3] <- kde2dRange[3] -3.0
     kde2dRange[4] <- kde2dRange[4] +3.0
 
-    dens <- getKde(pts,N=100,Lims=kde2dRange) 
+    dens <- getKde(pts,N=500,Lims=kde2dRange) 
+    
+    minZ <- (sapply(dens,min)[c('z')])
+    maxZ <- (sapply(dens,max)[c('z')])
+
     fillCols <- rev(brewer.pal(11,'Spectral'))	
     all_states<-map_data("state")
     emap <- ggplot()
+    
     emap <- emap+geom_raster(data = dens, aes(x=x, y=y,  fill=z, group=1)) + 
-		scale_fill_gradientn(colours=fillCols) + 
+		scale_fill_gradientn(breaks=c(minZ,maxZ), labels=c("Less Occ. ", "More Occ."), colours=fillCols) + 
 		coord_equal()
     emap <- emap + geom_path( data=all_states, aes(x=long, y=lat,group = group),colour="white")+theme_map()
 
-       emap
- 
+       #emap
+       ggsave(paste(name, "_density.pdf", sep=''), height=4, width=5)
+    
 }
 
 density_map("all_places")
